d6a9cc7bb55dc44921cce9230fc1beb2baf752ef
Saiyam Arora <saiyam.arora11@gmail.com>
2025-05-23 21:43:55 +0530

    intial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,6 @@ export default function Home() {
           priority
         />
         <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
           <li>Save and see your changes instantly.</li>
         </ol>
 
